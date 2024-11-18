162ddd340f476c1e803611cb3af373c18cb7da27
GHPaulina <pgomezh@unal.edu.co>
2024-11-17 20:21:39 -0500

    Taller

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -2,66 +2,50 @@ package taller3.televisores;
 
 public class Control {
 
-	    private TV televisor;
-
-	    public void enlazar(TV televisor) {
-	        this.televisor = televisor;
-	        televisor.setControl(this);
-	    }
-
-	    public void turnOn() {
-	        if (televisor != null) {
-	            televisor.turnOn();
-	        }
-	    }
-
-	    public void turnOff() {
-	        if (televisor != null) {
-	            televisor.turnOff();
-	        }
-	    }
-
-	    public void canalUp() {
-	        if (televisor != null) {
-	            televisor.canalUp();
-	        }
-	    }
-
-	    public void canalDown() {
-	        if (televisor != null) {
-	            televisor.canalDown();
-	        }
-	    }
-
-	    public void volumenUp() {
-	        if (televisor != null) {
-	            televisor.volumenUp();
-	        }
-	    }
-
-	    public void volumenDown() {
-	        if (televisor != null) {
-	            televisor.volumenDown();
-	        }
-	    }
-
-	    public void setCanal(int canal) {
-	        if (televisor != null) {
-	            televisor.setCanal(canal);
-	        }
-	    }
-
-	    public void setVolumen(int volumen) {
-	        if (televisor != null) {
-	            televisor.setVolumen(volumen);
-	        }
-	    }
-
-	    public TV getTelevisor() {
-	        return televisor;
-	    }
-
-	    public void setTelevisor(TV televisor) {
-	        this.televisor = televisor;
-	    }
+private TV tv;
+	
+	public void enlazar(TV tv) {
+		this.tv = tv;
+		tv.setControl(this);
 	}
+	
+	public void turnOn() {
+		this.tv.turnOn();
+	}
+	
+	public void turnOff() {
+		this.tv.turnOff();
+	}
+	
+	public void canalUp() {
+		this.tv.canalUp();
+	}
+	
+	public void canalDown() {
+		this.tv.canalDown();
+	}
+	
+	public void volumenUp() {
+		this.tv.volumenUp();
+	}
+	
+	public void volumenDown() {
+		this.tv.volumenDown();
+	}
+	
+	public void setCanal(int canal) {
+		this.tv.setCanal(canal);
+	}
+	
+	public void setVolumen(int volumen) {
+		this.tv.setVolumen(volumen);
+	}
+	
+	public TV getTv() {
+		return this.tv;
+	}
+	
+	public void setTv(TV tv) {
+		this.tv = tv;
+	}
+}
--- src/main/java/taller3/televisores/Marca.java
+++ src/main/java/taller3/televisores/Marca.java
@@ -2,20 +2,17 @@ package taller3.televisores;
 
 public class Marca {
 	
-	    private String nombre; 
-
-	    public Marca(String nombre) {
-	        this.nombre = nombre;
-	    }
-
-	    public String getNombre() {
-	        return nombre;
-	    }
-
-	    public void setNombre(String nombre) {
-	        this.nombre = nombre;
-	    }
+private String nombre;
+	
+	public Marca(String nombre) {
+		this.setNombre(nombre);
 	}
-
-
-
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
